16c908c74bc2549096a613d6f93ad5c44a59385c
Ernest103 <ernest0399@icloud.com>
2021-09-11 17:28:43 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //Q_5
+        Scanner sc = new Scanner(System.in);
+        String n1, n2;
+        int a, b, sum, sub, mul, quo;
+
+        System.out.print("What is the first number? ");
+        n1 = sc.nextLine();
+        System.out.print("What is the second number? ");
+        n2 = sc.nextLine();
+
+        a = Integer.parseInt(n1);
+        b = Integer.parseInt(n2);
+        sum = a + b;
+        sub = a - b;
+        mul = a * b;
+        quo = a / b;
+
+        System.out.println(n1 + " + " + n2 + " = " + sum + "\n" +
+                n1 + " - " + n2 + " = " + sub + "\n" +
+                n1 + " * " + n2 + " = " + mul + "\n" +
+                n1 + " / " + n2 + " = " + quo + "\n");
+    }
+}
